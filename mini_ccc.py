08216325af4214ca4b0529a8535d777cdb791a25
Evan6998 <hyf6998@icloud.com>
2025-09-12 16:22:56 -0400

    refactor dataset info and split generators: enhance _info method to include features and update _split_generators for proper data handling

--- mini_ccc.py
+++ mini_ccc.py
@@ -13,19 +13,39 @@ class MiniCleanedCommonCrawl(datasets.GeneratorBasedBuilder):
         """
         Should return a DatasetInfo object describing <string> type values for a url and it's corresponding text.
         """
-        return datasets.DatasetInfo(description=_DESCRIPTION)
+        return datasets.DatasetInfo(description=_DESCRIPTION, 
+            features=datasets.Features(
+                {
+                    "context": datasets.Value("string"),
+                    "url": datasets.Value("string"),
+                }
+        ),)
 
     def _split_generators(self, dl_manager: datasets.DownloadManager) -> List[datasets.SplitGenerator]:
         """
         Should return a SplitGenerator object which downloads your data and creates train and validation splits.
         """
-        return datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepaths": dl_manager.download_and_extract(_DATA_URL)}),
+        data = dl_manager.download([_DATA_URL])
+        train = datasets.SplitGenerator(
+            name=datasets.Split.TRAIN,
+            gen_kwargs={
+                "filepaths": data,
+            },
+        )
+        validation = datasets.SplitGenerator(
+            name=datasets.Split.VALIDATION,
+            gen_kwargs={
+                "filepaths": data,
+            },
+        )
+        return [train, validation]
     
     def _generate_examples(self, filepaths: List[str]) -> Iterator[Tuple[Any, Dict[str, str]]]:
         """
         Streams raw data from the downloaded file and yields tuples consisting of a unique ID and the url/cleaned text.
         Should call the functions you defined in homework.py. 
         """
+        _id = 0
         for filepath in filepaths:
             for url, html_text in homework.read_warc_file(filepath):
                 text = homework.html_to_text(str(html_text))
@@ -33,7 +53,10 @@ class MiniCleanedCommonCrawl(datasets.GeneratorBasedBuilder):
                 cleaned_nopii_text = homework.replace_pii(cleaned_text)
                 passes_check = homework.heuristic_quality_filter(cleaned_nopii_text)
                 if passes_check:
-                    yield url, cleaned_nopii_text
+                    yield str(_id), {
+                        "context": cleaned_nopii_text,
+                        "url": url
+                    }
  
 if __name__ == "__main__":   
     # Note: Calling load_dataset caches the processed dataset locally.
